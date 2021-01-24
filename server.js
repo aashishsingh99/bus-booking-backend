843ccb7fa5fcb1a189392f8debafd647d3a0875a
Aashish <singh.aashish2399@gmail.com>
2021-01-24 20:47:27 +0530

    added env

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express")
 const cors = require("cors")
 const body = require("body-parser")
 const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
 const secret = "my-secret"
 const app = express()
 const PORT = 5000
 
+require('dotenv').config()
+
 app.use(cors())
 app.use(body.json())
 app.use(body.urlencoded({extended:true}))
+
+mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('mongoDB connected');
+});
 
 app.listen(PORT, ()=> console.log(`server started on PORT ${PORT}`))
